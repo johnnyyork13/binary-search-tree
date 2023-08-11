6823535fd2ab29ac7c692efefcf827d905209ae2
johnnyyork13 <johnnyyork13@hotmail.com>
2023-08-11 19:28:42 -0400

    binary search practice

--- khan.js
+++ khan.js
@@ -1,35 +1,38 @@
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
+let guessList = [];
 
-function caesar(letter, adjust) {
-    let adjustedIndex;
-    for (let i = 0; i < alphabet.length; i++) {
-        const currentLetter = alphabet[i];
-        if (letter === currentLetter) {
-            if (i - adjust < 0) {
-                adjustedIndex = 26 + (i - adjust);
-            } else {
-                adjustedIndex = i - adjust;
-            }
-            return alphabet[adjustedIndex];
-        }
+// function binarySearch(num, range) {
+//     const guess = Math.trunc(Math.floor((range[0] + range[1]) / 2));
+//     guessList.push(guess);
+//     console.log(guess);
+//     if (guess === num) {
+//         return guess;
+//     } else if (guess > num) {
+//         return binarySearch(num, [range[0], guess - 1])
+//     } else if (guess < num) {
+//         return binarySearch(num, [guess + 1, range[1]])
+//     }
+//     return guess;
+// }
+
+// binarySearch(100, [0, 100]);
+
+
+function binarySearch(myNum, max, min = 1) {
+    const guess = Math.trunc(Math.floor((min + max) / 2));
+    console.log(guess);
+    if (guess === myNum) {
+        return guess;
+    } else if (guess > myNum) {
+        return binarySearch(myNum, guess - 1, min);
+    } else if (guess < myNum) {
+        return binarySearch(myNum, max, guess + 1);
     }
+    return guess;
 }
 
-const cypher = "gluhtlishjrvbadvyyplkaohavbyjpwolypzavvdlhrvuuleatlzzhnlzdpajoavcpnlulyljpwolyrlfdvykpzaolopkkluzftivsvmklhaoputfmhcvypalovsilpuluk";
-const cypherTwo = "vwduwljudeehghyhubwklqjlfrxogilqgsohdvhuhwxuqdqbeoxhsulqwviruydxowd qgdodupghvljqedvhgrqzklfkedqnbrxghflghrqldpvhwwlqjxsvdihkrxvhfr";
 
-let decoded = "";
-for (let x = 0; x < cypher.length; x++) {
-    decoded = decoded + caesar(cypher[x], 7);
-}
-console.log(decoded);
-decoded = "";
+const myNum = 10;
+const max = 100;
 
-for (let x = 0; x < cypherTwo.length; x++) {
-    decoded = decoded + caesar(cypherTwo[x], 3);
-}
-console.log(decoded);
-
-
-
-
+console.log(binarySearch(myNum, max));
+console.log(guessList);
